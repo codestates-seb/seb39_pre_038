95cd65c4d5fa48b3ec8143b2e976f6e46e1ce947
ds02168 <1995musso@gmail.com>
2022-08-28 22:17:22 +0900

    feat: 요구사항 2.1 2.6 서비스로직 구현

--- backend/src/main/java/com/pre_38/pre_project/question/service/QuestionService.java
+++ backend/src/main/java/com/pre_38/pre_project/question/service/QuestionService.java
@@ -1,7 +1,10 @@
 package com.pre_38.pre_project.question.service;
 
 import com.pre_38.pre_project.question.entity.Question;
+import com.pre_38.pre_project.question.repository.QuestionRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -10,14 +13,20 @@ import java.util.List;
 @Transactional
 @Service
 public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public QuestionService(QuestionRepository questionRepository){
+        this.questionRepository = questionRepository;
+    }
 
     //요구사항 2.1 + 2.6
     public Page<Question> findQuestions(int page, int size){
-        return null;
+        return questionRepository.findAll(PageRequest.of(page,size, Sort.by("questionId").descending()));
     }
 
     //요구사항 2.2
     public Question createQuestion(Question question){
+
         return null;
     }
 
