26f89270f2ca278de122dfef392695e4146c43b9
lkc9898 <104333737+lkc9898@users.noreply.github.com>
2022-08-29 02:39:39 +0900

    feat: 회원가입 초기 구현

--- backend/src/main/java/com/pre_38/pre_project/member/service/MemberService.java
+++ backend/src/main/java/com/pre_38/pre_project/member/service/MemberService.java
@@ -1,11 +1,27 @@
 package com.pre_38.pre_project.member.service;
 
+import com.pre_38.pre_project.member.dto.MemberSignupRequestDto;
 import com.pre_38.pre_project.member.entity.Member;
+import com.pre_38.pre_project.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
 
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public void signup(MemberSignupRequestDto signupDto) {
+        Member member = Member.builder()
+                .username(signupDto.getUsername())
+                .password(signupDto.getPassword())
+                .email(signupDto.getEmail())
+                .build();
+        memberRepository.save(member);
+    }
     //멤버 찾기용
     public Member findMember(String memberName){
         return null;
