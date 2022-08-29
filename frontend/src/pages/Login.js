a5e248fc69346a9dc139d914375f310ca447a733
YHJ96 <yhj878812@naver.com>
2022-08-30 01:55:18 +0900

    design: 로그인 화면 UI & CSS 구현 완료

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import SvgIcon from '../components/SvgIcon';
+import styles from './Login.module.css';
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <main className={styles.container}>
+      <SvgIcon name="stackOverFlow" />
+
+      <form className={styles.btnWrap}>
+        <button type="button">
+          <SvgIcon name="google" />
+          Log in with Google
+        </button>
+        <button type="button">
+          <SvgIcon name="gitHub" />
+          Log in with Google
+        </button>
+        <button type="button">
+          <SvgIcon name="faceBook" />
+          Log in with Google
+        </button>
+      </form>
+
+      <footer className={styles.textWrap}>
+        <div>
+          Don’t have an account? <em>Sign up</em>
+        </div>
+        <div>
+          Are you an employer?
+          <em>Sign up on Talent</em>
+          <SvgIcon name="share" className={styles.test} />
+        </div>
+      </footer>
+    </main>
+  );
 }
 
 export default Login;
