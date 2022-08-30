c62027dcb88a237a4eab73439c3f28a28eec26f5
YHJ96 <yhj878812@naver.com>
2022-08-30 16:47:17 +0900

    feat: hamberger 아이콘 toggle 기능 추가 완료

--- frontend/src/components/Nav/Nav.js
+++ frontend/src/components/Nav/Nav.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Nav.module.css';
 import SpriteIcon from '../SpriteIcon/SpriteIcon';
 
 function Nav() {
+  const [toggle, setToggle] = useState(false);
+  const toggleHamberger = () => setToggle(!toggle);
+
   return (
     <nav className={styles.container}>
+      <div
+        className={toggle ? styles.hamberger_on : styles.hamberger_off}
+        onClick={toggleHamberger}
+        aria-hidden="true"
+        role="button"
+        tabIndex={0}
+      >
+        <span />
+      </div>
+
       <SpriteIcon name="logo">
         <Link to="/">Questions</Link>
       </SpriteIcon>
