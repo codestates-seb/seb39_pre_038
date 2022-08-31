8728e1bc15405a32921e32b12fe785b1e6ad1edc
YHJ96 <yhj878812@naver.com>
2022-09-01 00:00:30 +0900

    feat: 질문페이지 초기 설정 완료

---
+++ frontend/src/components/AsideNav/AsideNav.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import styles from './AsideNav.module.css';
+
+function AsideNav() {
+  return <div>AsideNav</div>;
+}
+
+export default AsideNav;
--- frontend/src/pages/Questions/Questions.js
+++ frontend/src/pages/Questions/Questions.js
@@ -1,14 +1,13 @@
-import React, { useRef } from 'react';
-import Editor from '../../components/Editor/Editor';
+import React from 'react';
+import AsideNav from '../../components/AsideNav/AsideNav';
 import './Questions.module.css';
 
 function Questions() {
-  const editorRef = useRef(null);
-
   return (
-    <React.Fragment key={null}>
-      <Editor type="write" ref={editorRef} />
-    </React.Fragment>
+    <main>
+      <AsideNav />
+      <section>정보</section>
+    </main>
   );
 }
 
