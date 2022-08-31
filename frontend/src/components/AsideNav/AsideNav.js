d65b8138da670d2768d40f759f955abe04720885
YHJ96 <yhj878812@naver.com>
2022-09-01 01:22:06 +0900

    design: 사이드 네비게이션 UI & CSS 구현 완료

--- frontend/src/components/AsideNav/AsideNav.js
+++ frontend/src/components/AsideNav/AsideNav.js
@@ -1,8 +1,45 @@
 import React from 'react';
-// import styles from './AsideNav.module.css';
+import styles from './AsideNav.module.css';
+import SvgIcon from '../SvgIcon/SvgIcon';
 
 function AsideNav() {
-  return <div>AsideNav</div>;
+  return (
+    <aside>
+      <nav className={styles.container}>
+        <ul>
+          Questions
+          <li>
+            <SvgIcon name="earth" />
+            Questions
+          </li>
+          <div className={styles.textWrap}>
+            <li>Tags</li>
+            <li>Users</li>
+            <li>Companies</li>
+          </div>
+        </ul>
+
+        <ul>
+          COLLECTIVES
+          <li>
+            <SvgIcon name="star" />
+            Explore Collectives
+          </li>
+        </ul>
+
+        <ul>
+          TEAMS
+          <li>
+            <div className={styles.icon}>
+              <SvgIcon name="basket" />
+              <SvgIcon name="shield" />
+            </div>
+            Create free Team
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
 }
 
 export default AsideNav;
