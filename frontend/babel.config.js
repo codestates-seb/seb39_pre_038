abe9d660c03e731491918f4b37f2b2032a9d01d5
kal1777 <kimnayul17@gmail.com>
2022-08-31 21:23:24 +0900

    feat: .gitignore 수정

--- frontend/babel.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  // presets : 바벨 기본 셋팅 (ES6 -> ES5, react -> ES5)
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-};
--- scripts/server_start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd /home/ubuntu/seb39_pre_038_new
-sudo nohup java -jar pre_project-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
