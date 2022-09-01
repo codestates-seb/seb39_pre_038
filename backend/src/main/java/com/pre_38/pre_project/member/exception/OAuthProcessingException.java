9210fe5f96b3d61e8ec20ec6b88b8de19c35a4ab
lkc9898 <104333737+lkc9898@users.noreply.github.com>
2022-09-02 00:42:10 +0900

    fix: backend 패키지 내 중복 클래스 제거

--- backend/src/main/java/com/pre_38/pre_project/member/exception/OAuthProcessingException.java
+++
@@ -1,7 +0,0 @@
-package com.pre_38.pre_project.member.exception;
-
-public class OAuthProcessingException extends RuntimeException{
-    public OAuthProcessingException(String message) {
-        super(message);
-    }
-}
