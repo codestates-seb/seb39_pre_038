a710cb5188242cac5fda6f495b1d93d2f50f1d7f
양희준 <88187322+YHJ96@users.noreply.github.com>
2022-09-02 03:38:40 +0900

    feat: 답변페이지 추가 완료  (#51)
    * feat: logo path 경로 추가 완료
    
    * feat: proxy 설정 완료
    
    * feat: stackOverFlow 아이콘 크기 커스텀 설정 추가 완료
    
    * design: 네비게이션 반응형 추가 완료
    
    * design: 질문페이지 반응형 추가 완료
    
    * fix: 반응형 분기점 수정 완료
    
    * fix: 라우터 경로 수정 완료
    
    * design: 버튼 UI & hover 효과 추가 완료
    
    * fix: search 아이콘 색 수정 완료
    
    * feat: 네비게이션 UI & CSS 구현 완료
    
    * refactor: 사용하지 않는 속성 제거 완료
    
    * fix: input 길이 문제 수정 완료
    
    * feat: 질문게시판 레이아웃 구현 완료
    
    * design: 질문게시판 UI & CSS 구현 완료
    
    * feat: 질문 form UI & CSS 구현 완료
    
    * design: 질문 form 반응형 구현 완료
    
    * design: 햄버거 아이콘 비활성화
    
    * feat: ask 네비게이션 경로 추가 완료
    
    * feat: AskQuestion 컴포넌트 경로에 추가 완료
    
    * feat: Detail 컴포넌트 추가 완료
    
    * feat: notFound 아이콘 추가 완료
    
    * feat: NotFound UI & CSS & 반응형 추가 완료
    
    * feat: 경로에 없는 주소 예외처리 라우팅 구현 완료
    
    * feat: 중첩 라우터 기본 설정 완료
    
    * feat: 네비게이션 useParams 경로 확인 기능 구현 완료
    
    * design: 답변 게시판 레이아웃 완료
    
    * fix: p 태그 오류 수정 완료
    
    * feat: 답변페이지 추가 완료
    
    * design: 커서 포인터 추가 완료

---
+++ frontend/src/components/Question/Question.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './Question.module.css';
+
+function Question({ path = '고유 아이디' }) {
+  const navigate = useNavigate();
+  // Detail 경로로 이동하기 위한 함수
+  const handleOnClick = () => {
+    navigate(`/${path}`);
+  };
+  return (
+    <div
+      className={styles.container}
+      onClick={handleOnClick}
+      role="button"
+      aria-hidden="true"
+    >
+      <div className={styles.stats}>
+        <div>0 votes</div>
+        <div>2 answer</div>
+        <div>26 views</div>
+      </div>
+      <div className={styles.content}>
+        <h2>Using a button hover to increase margin with javascript</h2>
+        <p>
+          Im trying to append parts of a dataframe to a json file in order to
+          efficiently store new data in already existing files. I tried the
+          following (and many other things), but this does not lead to a nicely
+          readable json file what what what what v vwhat whatsss
+        </p>
+        <div>
+          <img alt="avatar" />
+          <span className={styles.userId}>YHJ96</span>
+          <span className={styles.timeAgo}>answered 45 secs ago</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Question;
