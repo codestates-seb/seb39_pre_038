5044759760b2155c124a368606876815301b1f05
YHJ96 <yhj878812@naver.com>
2022-09-01 22:07:47 +0900

    feat: 질문게시판 레이아웃 구현 완료

--- frontend/src/pages/Questions/Questions.js
+++ frontend/src/pages/Questions/Questions.js
@@ -6,7 +6,18 @@ function Questions() {
   return (
     <main className={styles.container}>
       <AsideNav />
-      <section className={styles.content}>정보</section>
+      <section className={styles.content}>
+        <div className={styles.mainber}>
+          <h1>All Questions</h1>
+          <button type="button">Ask Question</button>
+        </div>
+        <div className={styles.subber}>
+          <em>22,960,505 questions</em>
+          <button type="button">Filter</button>
+        </div>
+
+        <div className={styles.questions}>Question</div>
+      </section>
     </main>
   );
 }
