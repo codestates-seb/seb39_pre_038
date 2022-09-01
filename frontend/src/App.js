adebba398efe4dd1b2194cf7b882a74b4543d7ae
YHJ96 <yhj878812@naver.com>
2022-09-02 00:35:15 +0900

    feat: AskQuestion 컴포넌트 경로에 추가 완료

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav/Nav';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
 import Questions from './pages/Questions/Questions';
+import AskQuestion from './components/AskQuestion/AskQuestion';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Questions />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/ask" element={<AskQuestion />} />
       </Routes>
     </React.Fragment>
   );
