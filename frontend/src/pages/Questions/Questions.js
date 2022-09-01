c1711d9e78c9b13cac4d1192df186a5dd8e59f35
YHJ96 <yhj878812@naver.com>
2022-09-01 23:49:48 +0900

    design: 질문게시판 UI & CSS 구현 완료

--- frontend/src/pages/Questions/Questions.js
+++ frontend/src/pages/Questions/Questions.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import AsideNav from '../../components/AsideNav/AsideNav';
 import styles from './Questions.module.css';
+import Question from '../../components/Question/Question';
 
 function Questions() {
   return (
     <main className={styles.container}>
       <AsideNav />
       <section className={styles.content}>
-        <div className={styles.mainber}>
-          <h1>All Questions</h1>
-          <button type="button">Ask Question</button>
-        </div>
-        <div className={styles.subber}>
-          <em>22,960,505 questions</em>
-          <button type="button">Filter</button>
+        <div className={styles.titleWrap}>
+          <div className={styles.mainber}>
+            <h1>All Questions</h1>
+            <button type="button">Ask Question</button>
+          </div>
+          <div className={styles.subber}>
+            <em>22,960,505 questions</em>
+            <button type="button">Filter</button>
+          </div>
         </div>
 
-        <div className={styles.questions}>Question</div>
+        <div className={styles.questions}>
+          <Question />
+        </div>
       </section>
     </main>
   );
