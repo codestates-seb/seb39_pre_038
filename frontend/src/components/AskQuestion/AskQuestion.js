f844c5c78b87b42aa85bff53dfd0dd93854d12c6
YHJ96 <yhj878812@naver.com>
2022-09-02 16:00:26 +0900

    feat: 텍스트 전송 페이지 레이아웃 완료

--- frontend/src/components/AskQuestion/AskQuestion.js
+++ frontend/src/components/AskQuestion/AskQuestion.js
@@ -1,17 +1,46 @@
 import React from 'react';
+import styles from './AskQuestion.module.css';
+
 import Editor from '../Editor/Editor';
 
 function AskQuestion() {
   return (
-    <div>
-      <Editor
-        type="write"
-        previewStyle="vertical"
-        height="400px"
-        initialEditType="markdown"
-      />
-    </div>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.contentHeader}>
+          <h1>Ask a public question</h1>
+        </div>
+        <div className={styles.editorWrap}>
+          <div className={styles.editorHeader}>
+            <h3>Title</h3>
+            <p>안녕하세요</p>
+            <input type="text" />
+          </div>
+
+          <div className={styles.editorBody}>
+            <h3>Body</h3>
+            <p>안녕하세요</p>
+            <div className={styles.editor}>
+              <Editor type="write" previewStyle="vertical" height="400px" />
+            </div>
+          </div>
+
+          <div className={styles.view}>
+            <Editor type="View" />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
 export default AskQuestion;
+
+/*
+  <Editor
+    type="write"
+    previewStyle="vertical"
+    height="400px"
+    initialEditType="markdown"
+  />
+*/
