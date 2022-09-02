a410e71d14a5db276e3ba0a23b591d2d6517f551
YHJ96 <yhj878812@naver.com>
2022-09-02 16:45:26 +0900

    feat: 텍스트 전송 페이지 UI & CSS 반응형 구현 완료

--- frontend/src/components/AskQuestion/AskQuestion.js
+++ frontend/src/components/AskQuestion/AskQuestion.js
@@ -13,22 +13,28 @@ function AskQuestion() {
         <div className={styles.editorWrap}>
           <div className={styles.editorHeader}>
             <h3>Title</h3>
-            <p>안녕하세요</p>
-            <input type="text" />
+            <p>
+              Be specific and imagine you`re asking a question to another person
+            </p>
+            <input
+              type="text"
+              placeholder="e.g is there an R function someone would need to answer your question"
+            />
           </div>
 
           <div className={styles.editorBody}>
             <h3>Body</h3>
-            <p>안녕하세요</p>
-            <div className={styles.editor}>
-              <Editor type="write" previewStyle="vertical" height="400px" />
-            </div>
+            <p>
+              Include all the information someone would need to answer you
+              question
+            </p>
+            <Editor type="write" previewStyle="vertical" height="300px" />
           </div>
-
-          <div className={styles.view}>
-            <Editor type="View" />
-          </div>
+          <Editor type="View" />
         </div>
+        <button className={styles.postBtn} type="button">
+          Review your question
+        </button>
       </div>
     </section>
   );
