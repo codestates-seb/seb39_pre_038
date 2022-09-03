2b4d9bde1f6f74d1fb844e9e2c4d009c974672b9
ds02168 <1995musso@gmail.com>
2022-09-03 18:36:50 +0900

    feat: Question, Reply에 Update 기능 추가

--- src/main/java/com/pre_38/pre_project/question/dto/QuestionDto.java
+++ src/main/java/com/pre_38/pre_project/question/dto/QuestionDto.java
@@ -33,19 +33,14 @@ public class QuestionDto {
     @Getter
     @AllArgsConstructor
     public static class Patch{
-        private long questionId;
-
         @NotBlank(message = "제목은 공백이 아니어야 합니다.")
         private String title;
 
         @NotEmpty(message = "내용을 입력하셔야 합니다.")
         private String content;
 
-        private String name;
-
-        public void setQuestionId(long questionId){
-            this.questionId = questionId;
-        }
+        @Email
+        private String email;
     }
 
     @Getter
