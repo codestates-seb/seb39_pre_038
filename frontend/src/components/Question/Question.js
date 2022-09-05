3fdca0c2845fa8152fc58cdd7aec70749b33f1fb
YHJ96 <yhj878812@naver.com>
2022-09-05 15:00:57 +0900

    refactor: 통신 더미 데이터 예외 처리 완료

--- frontend/src/components/Question/Question.js
+++ frontend/src/components/Question/Question.js
@@ -2,7 +2,21 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Question.module.css';
 
-function Question({ path = '고유 아이디' }) {
+const defaultData = {
+  title: 'Using a button hover to increase margin with javascript',
+  content: `Im trying to append parts of a dataframe to a json file in order to
+  efficiently store new data in already existing files. I tried the
+  following (and many other things), but this does not lead to a nicely
+  readable json file.`,
+  date: '45 secs ago',
+  member: {
+    username: 'YHJ96',
+    avatar: '',
+  },
+  replies: 0,
+};
+
+function Question({ path = '', userData = defaultData }) {
   const navigate = useNavigate();
   // Detail 경로로 이동하기 위한 함수
   const handleOnClick = () => {
@@ -17,21 +31,16 @@ function Question({ path = '고유 아이디' }) {
     >
       <div className={styles.stats}>
         <div>0 votes</div>
-        <div>2 answer</div>
+        <div>{userData.replies} answer</div>
         <div>26 views</div>
       </div>
       <div className={styles.content}>
-        <h2>Using a button hover to increase margin with javascript</h2>
-        <p>
-          Im trying to append parts of a dataframe to a json file in order to
-          efficiently store new data in already existing files. I tried the
-          following (and many other things), but this does not lead to a nicely
-          readable json file what what what what v vwhat whatsss
-        </p>
+        <h2>{userData.title}</h2>
+        <p>{userData.content}</p>
         <div>
-          <img alt="avatar" />
-          <span className={styles.userId}>YHJ96</span>
-          <span className={styles.timeAgo}>answered 45 secs ago</span>
+          <img src={userData.member.avatar} alt="avatar" />
+          <span className={styles.userId}>{userData.member.username}</span>
+          <span className={styles.timeAgo}>{userData.date}</span>
         </div>
       </div>
     </div>
