33fcfbd33c447cc4c7fe9d7226893563315bd63c
Jaewon Kim <99641988+Mark1237200@users.noreply.github.com>
2022-09-05 12:05:00 +0900

    fix: 답변 게시판 css 마진 제거 & 가운데 정렬 수정 (#71)
    * Design: Replies 페이지 CSS 추가 및 CRD 기능 수정
    
    * feat: Replies 페이지 CRD 기능 추가 완료
    
    * feat: Replies CRD 기능 추가 완료
    
    * feat: Replies Update 기능 추가 완료
    
    * fix: Replies css 마진제거, 가운데 정렬
    
    Co-authored-by: Mark1237200 <“0915kimjw@gmail.com”>
    Co-authored-by: 양희준 <88187322+YHJ96@users.noreply.github.com>

--- frontend/src/components/Replies/Replies.js
+++ frontend/src/components/Replies/Replies.js
@@ -1,25 +1,19 @@
 import React, { useRef, useEffect, useState } from 'react';
 import axios from 'axios';
 import Editor from '../Editor/Editor';
-import style from './Replies.module.css';
+import styles from './Replies.module.css';
 import Reply from '../Reply/Reply';
 
 function Replies() {
-  const [totalAnswer, setTotalAnswer] = useState();
-  const [queId, setQueId] = useState();
-  const [ansId, setAnsId] = useState();
-  const [replies, setReplies] = useState();
+  const [totalAnswer, setTotalAnswer] = useState(0);
+  const [queId, setQueId] = useState(0);
+  const [ansId, setAnsId] = useState(0);
+  const [replies, setReplies] = useState(null);
 
   const viewRef = useRef();
   const editorRef = useRef();
   const viewerRef = useRef();
   const editRef = useRef();
-
-  // 미리보기 기능
-
-  // 1. Writer의 onChange 이벤트를 생성한다. [X]
-  // 2. Writer의 onChange가 일어날때 마다 Viewer의 값을 갱신한다. [X]
-  // 3. Viewer의 Ref의 값에 접근해서 setMarkDown()의 메소드를 불어와서 값을 갱신한다. [X]
 
   const onChangeIntroFunction = () => {
     const value = editorRef.current.getInstance().getHTML();
@@ -33,8 +27,7 @@ function Replies() {
 
   useEffect(() => {
     axios
-      // .get(`/questions/${queId}`)
-      .get('http://localhost:8080/data')
+      .get(`/questions/${queId}`)
       .then((res) => {
         console.log(res.data);
         setQueId(res.data.questionId);
@@ -44,14 +37,6 @@ function Replies() {
       })
       .catch((e) => console.log('somethign wrong:', e));
   }, []);
-
-  // POST, UPDATE 기능
-
-  // 1. POST 버튼을 눌렀을 때 onClick() 이벤트를 생성한다. [X]
-  // 2. POST의 body에 데이터를 보내는데 content: WriterRef.current.getInstance().getHTML(); 서버에 전송한다. [X]
-  // 3. POST를 보내고 받아온 응답 데이터 state에 갱신한다. [X]
-  // 4. UPDATE 게시물의 배열 인덱스에 접근해서 해당 content의 내용을 내가 작성한  WriterRef.current.getInstance().getHTML()로 갱신한다. [X]
-  // 5. useEffect(() => { 새로고침하면 어차피 그대로 남아있습니다 저희가 서버에 전송했으니깐요 }, []) [X]
 
   const posting = () => {
     axios
@@ -66,7 +51,6 @@ function Replies() {
       .catch((error) => {
         console.log(error);
       });
-    window.location.reload();
   };
 
   const patch = () => {
@@ -77,7 +61,7 @@ function Replies() {
   };
 
   const editing = (
-    <div className={style.editor}>
+    <div className={styles.editor}>
       <div>
         <Editor
           type="write"
@@ -92,7 +76,7 @@ function Replies() {
         <Editor type="view" ref={viewerRef} />
       </div>
       <button
-        className={style.btn}
+        className={styles.btn}
         type="button"
         aria-label="Editing"
         onClick={() => {
@@ -106,13 +90,12 @@ function Replies() {
 
   const AnswerDeleting = () => {
     axios.delete(`/questions/${queId}/${ansId}`);
-    window.location.reload();
   };
 
   return (
-    <main className={style.container}>
-      <section className={style.content}>
-        <div className={style.h3}>
+    <main className={styles.container}>
+      <section className={styles.content}>
+        <div className={styles.h3}>
           <h3>{totalAnswer} Answer</h3>
           <span>
             Sorted by:
@@ -134,7 +117,7 @@ function Replies() {
           ))}
 
         <article>
-          <h1 className={style.UrAnswer}>Your Answer</h1>
+          <h1 className={styles.UrAnswer}>Your Answer</h1>
 
           <div>
             <Editor
@@ -151,7 +134,7 @@ function Replies() {
           </div>
 
           <button
-            className={style.btn}
+            className={styles.btn}
             type="button"
             aria-label="Posting"
             onClick={() => {
