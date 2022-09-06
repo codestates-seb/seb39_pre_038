5930b768705bda1bc1e5f3c1a8a8a2dc466835ae
lkc9898 <104333737+lkc9898@users.noreply.github.com>
2022-09-06 16:40:20 +0900

    fix: @RequestBody, @Valid 추가

--- src/main/java/com/pre_38/pre_project/member/dto/UserRequestDto.java
+++ src/main/java/com/pre_38/pre_project/member/dto/UserRequestDto.java
@@ -3,8 +3,11 @@ package com.pre_38.pre_project.member.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @AllArgsConstructor
 public class UserRequestDto {
+    @NotNull
     private String request;
 }
