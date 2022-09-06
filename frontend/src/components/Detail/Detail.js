47ce947692de586145b9ea33a14fb2f24fa17c51
YHJ96 <yhj878812@naver.com>
2022-09-06 16:10:49 +0900

    refactor: useFetch 적용 완료

--- frontend/src/components/Detail/Detail.js
+++ frontend/src/components/Detail/Detail.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useRef } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 import styles from './Detail.module.css';
+import { useFetch } from '../../hooks/index';
 import { DETAIL_GET_QUESTION, DELETE_QUESTION } from '../../utils/api';
 import Editor from '../Editor/Editor';
 import Replies from '../Replies/Replies';
@@ -12,25 +13,12 @@ function Detail() {
   const navigate = useNavigate();
   const editorRef = useRef(null);
   const viewRef = useRef(null);
-  const [data, setData] = useState({});
-  const [isLoding, setIsLoding] = useState(true);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    axios
-      .get(DETAIL_GET_QUESTION(id))
-      .then((res) => {
-        setData(res.data);
-        setIsLoding(false);
-      })
-      .catch((err) => {
-        setIsLoding(false);
-        setError(err.message);
-      });
-  }, [id]);
+  const { data, isLoding, error } = useFetch(DETAIL_GET_QUESTION(id));
 
   if (isLoding) return <div>Loding</div>;
   if (error) return <div>{error}</div>;
+
+  const handleAskBtnOnClick = () => navigate('/ask');
 
   const handleOnDelete = () => {
     axios
@@ -57,7 +45,9 @@ function Detail() {
       <div className={styles.questionWrap}>
         <div className={styles.questionHeader}>
           <h1>{data.data.title}</h1>
-          <button type="button">Ask Question</button>
+          <button type="button" onClick={handleAskBtnOnClick}>
+            Ask Question
+          </button>
         </div>
         <div className={styles.date}>
           <span>
